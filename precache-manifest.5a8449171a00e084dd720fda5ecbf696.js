98b45f8a5858a769386a6c707878a6d7dd951dfb
26erika <develop.erika@gmail.com>
2019-11-08 16:26:43 -0600

    Se le quita rama master de deploy package.json

---
+++ precache-manifest.5a8449171a00e084dd720fda5ecbf696.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d295e98ea403e87a921b54a148f0cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f130f2efbe17e7771c7e",
+    "url": "/static/css/main.5b078849.chunk.css"
+  },
+  {
+    "revision": "bf515e789d338c8bfd50",
+    "url": "/static/js/2.89f15cdd.chunk.js"
+  },
+  {
+    "revision": "f130f2efbe17e7771c7e",
+    "url": "/static/js/main.c271ed8b.chunk.js"
+  },
+  {
+    "revision": "70327af8c84931932d34",
+    "url": "/static/js/runtime-main.98592f35.js"
+  }
+]);
